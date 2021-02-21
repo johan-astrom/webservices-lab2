9c57ef05e0f3b9a41a3814bee42a01b215efa3ec
Johan Astrom <johan.astrom89@gmail.com>
2021-02-21 13:44:44 +0100

    Added JsonProperty elements in AuthorName

--- src/main/java/com/johanastrom/webserviceslab2/dtos/AuthorName.java
+++ src/main/java/com/johanastrom/webserviceslab2/dtos/AuthorName.java
@@ -2,6 +2,6 @@ package com.johanastrom.webserviceslab2.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record AuthorName(@JsonProperty String firstName,
-                         @JsonProperty String lastName) {
+public record AuthorName(@JsonProperty("firstName") String firstName,
+                         @JsonProperty("lastName") String lastName) {
 }
