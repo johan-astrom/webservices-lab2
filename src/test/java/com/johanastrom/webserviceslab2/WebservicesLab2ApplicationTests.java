2b32f546a990e3686e2d1d7b35ed7ac6a649a1c8
Johan Astrom <johan.astrom89@gmail.com>
2021-02-21 16:38:52 +0100

    Added tests

--- src/test/java/com/johanastrom/webserviceslab2/WebservicesLab2ApplicationTests.java
+++ src/test/java/com/johanastrom/webserviceslab2/WebservicesLab2ApplicationTests.java
@@ -1,13 +1,31 @@
 package com.johanastrom.webserviceslab2;
 
+import com.johanastrom.webserviceslab2.entities.Author;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebservicesLab2ApplicationTests {
 
+    @Autowired
+    private TestRestTemplate testClient;
+
+    @LocalServerPort
+    private int port;
+
+    // TODO: 2021-02-21 Lägg  till application.properties i test
     @Test
     void contextLoads() {
+        var result = testClient.getForEntity("http://localhost:" + port + "/authors", Author[].class);
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody().length).isGreaterThan(0);
+
     }
 
 }
