e0cc8685f88bb371c961b3b06eadc282adcc0d5d
Johan Astrom <johan.astrom89@gmail.com>
2021-02-22 18:05:07 +0100

    Added AuthorController test class

---
+++ src/test/java/com/johanastrom/webserviceslab2/controllers/AuthorController.java
@@ -0,0 +1,13 @@
+package com.johanastrom.webserviceslab2.controllers;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthorController {
+
+    @Test
+    public void getAllAuthorsShouldReturnListOfAuthorObjects(){
+
+
+    }
+
+}
